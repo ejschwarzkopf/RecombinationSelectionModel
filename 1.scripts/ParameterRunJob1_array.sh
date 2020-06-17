e13a4d6edf16ca5fe70edf174bda8c266bee320a
Enrique(mac) <ejschwarzkopf@gmail.com>
2020-06-16 18:10:02 -0700

    Job array files made

---
+++ 1.scripts/ParameterRunJob1_array.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --job-name=ParameterRun1_array
+#SBATCH --partition=popgenom,kamiak,cas,cahnrs
+#SBATCH --output=/data/cornejo/projects/e.jimenezschwarzkop/LDModel/1.scripts/1.log/ParameterRun1_array.out
+#SBATCH --error=/data/cornejo/projects/e.jimenezschwarzkop/LDModel/1.scripts/1.log/ParameterRun1_array.err
+#SBATCH --time=7-00:00:00
+#SBATCH --nodes=1
+#SBATCH --workdir="/data/cornejo/projects/e.jimenezschwarzkop/LDModel"
+#SBATCH --array=1-13440:1# Number of jobs, in steps of 1
+
+i=${SLURM_ARRAY_TASK_ID}
+
+module load r/3.6.3
+
+Rscript --vanilla 1.scripts/ParameterRun1.R ${i} 3.aux/Parameters_run1.txt
